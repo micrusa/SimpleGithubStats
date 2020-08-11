91bd194bb0aee7601c193cea9671eeed9ed6e740
Miguel Cruces <micrusa.com@gmail.com>
2020-08-11 22:12:01 +0200

    Add a method to check if it's rate limited

--- app/src/main/java/me/micrusa/githubstats/utils/stats/StatsData.java
+++ app/src/main/java/me/micrusa/githubstats/utils/stats/StatsData.java
@@ -34,6 +34,10 @@ public abstract class StatsData {
     public boolean exists(){
         return errorCode != 404;
     }
+    
+    public boolean isRateLimited(){
+        return errorCode == 403;
+    }
 
     public void addRunnable(Runnable r){
         if(response != null) {
